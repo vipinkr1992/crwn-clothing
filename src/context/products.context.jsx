81acbeeb6d83e04cdeef8d430f22da48b8e495ac
vipinkr1992 <vipinkr1992@proton.me>
2025-03-20 12:05:50 +0530

    set data in firestore

--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -1,13 +1,19 @@
-import { createContext, use, useState } from "react";
-import PRODUCTS from '../shop-data.json';
+import { createContext, useEffect, useState } from "react";
+import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils';
+
+import SHOP_DATA from '../shop-data.js';
 
 export const ProductsContext = createContext({
-    products:[]
+    products: []
 })
 
-export const ProductsProvider = ({children})=>{
-    const [products,setProducts] = useState(PRODUCTS);
-    const value = {products};
+
+export const ProductsProvider = ({ children }) => {
+    const [products, setProducts] = useState([]);
+    const value = { products };
+    useEffect(() => {
+        addCollectionAndDocuments('collections', SHOP_DATA);
+    }, []);
     return (
         <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
     )
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,7 +9,14 @@ import {
   signOut,
   onAuthStateChanged
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -36,6 +43,22 @@ export const signInWithGoogleRedirect = () =>
   signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const batch = writeBatch(db);
+  const collectionRef = collection(db, collectionKey);
+  
+  objectsToAdd.forEach((object) => {
+     const docRef = doc(collectionRef, object.title.toLowerCase());
+     batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log('done');
+};
 
 export const createUserDocumentFromAuth = async (
   userAuth,
