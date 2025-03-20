ce682b668877a2161e6f418aec8457dfa134f499
vipinkr1992 <vipinkr1992@proton.me>
2025-03-20 12:40:32 +0530

    category preview

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,8 +1,6 @@
-import { useContext, Fragment } from 'react';
+import { useContext } from 'react';
 
-import ProductCard from '../../components/product-card/product-card.component';
-
-
+import CategoryPreview from '../../components/category-preview/category-preview.component';
 
 import './shop.styles.scss';
 import { CategoriesContext } from '../../context/categories.context';
@@ -11,18 +9,12 @@ const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className='products-container'>
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
+    <div className='shop-container'>
+      {Object.keys(categoriesMap).map((key) => {
+        const products = categoriesMap[key];
+        return <CategoryPreview key={key} title={key} products={products} />;
+      })}
+    </div>
   );
 };
 
