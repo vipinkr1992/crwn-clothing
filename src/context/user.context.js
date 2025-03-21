5912b867d66c5a2baea80f9481cd7875cafd9b61
vipinkr1992 <vipinkr1992@proton.me>
2025-03-21 13:11:50 +0530

    userreducer with context

--- src/context/user.context.js
+++ src/context/user.context.js
@@ -1,24 +1,46 @@
-import { createContext, useState,useEffect } from "react";
+import { createContext, useState, useEffect,useReducer } from "react";
 import { createUserDocumentFromAuth, onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
-    currentUser: null,
-    setCurrentUser: () => null
+  currentUser: null,
+  setCurrentUser: () => null
 })
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+export const userReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+   case USER_ACTION_TYPES.SET_CURRENT_USER:
+    return{
+      ...state,
+      currentUser:payload
+    }
+   default:
+    throw new Error(`Unhandled type ${type} in userReducer`)
+  }
+}
+const INITIAL_STATE = {
+  currentUser: null
+}
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const value = { currentUser, setCurrentUser };
-    useEffect(() => {
-        const unsubscribe = onAuthStateChangedListener((user) => {
-          if (user) {
-            createUserDocumentFromAuth(user);
-          }
-          setCurrentUser(user);
-        });
-    
-        return unsubscribe;
-      }, []);
+ // const [currentUser, setCurrentUser] = useState(null);
+  
+  const [{currentUser},dispatch] =  useReducer(userReducer,INITIAL_STATE);
+  const setCurrentUser = (user) => {
+    dispatch({type:USER_ACTION_TYPES.SET_CURRENT_USER,payload:user})
+  }
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      setCurrentUser(user);
+    });
 
-    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+    return unsubscribe;
+  }, []);
+  const value = { currentUser,setCurrentUser };
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
