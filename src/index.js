5548c62c7bde235cf78133fb504d018c9767d2a6
vipinkr1992 <vipinkr1992@proton.me>
2025-03-21 15:44:43 +0530

    create store and inject into index js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './context/user.context';
 import { CartProvider } from './context/cart.context';
 import { CategoriesProvider } from './context/categories.context';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <UserProvider>
         <CategoriesProvider>
@@ -20,6 +23,7 @@ root.render(
         </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
