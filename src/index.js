f19a403ab09eab2d6e07aeb6a3b45b76e6cc6dbb
vipinkr1992 <vipinkr1992@proton.me>
2025-03-24 11:38:57 +0530

    redux persist cart items on refresh

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import { Provider } from 'react-redux';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './store/store';
+import { persistor, store } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,33 @@
 import { compose,createStore,applyMiddleware } from "redux";
 import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { rootReducer } from "./root-reducer";
 
-const middleware = [logger];
-
-export const composeEnhancers = compose(applyMiddleware(...middleware));
-
-export const store = createStore(rootReducer,undefined,composeEnhancers);
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+    Boolean
+  );
+  
+  const composeEnhancer =
+    (process.env.NODE_ENV !== 'production' &&
+      window &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
+  
+  const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user'],
+  };
+  
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
+  
+  const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+  
+  export const store = createStore(
+    persistedReducer,
+    undefined,
+    composedEnhancers
+  );
+  
+  export const persistor = persistStore(store);
